9b50f0ece594f7e5550a7d08a10c97e62ec21300
Lisa Kilbinger <lizkil.dev@gmail.com>
2022-06-07 17:12:51 +0200

    some code cleaning

--- server.js
+++ server.js
@@ -28,10 +28,10 @@ app.get('/', (request, response) => {
 	response.sendFile(__dirname + '/index.html')
 })
 
-app.get('/api/:rapperName', (request, response) => {
-	const rappersName = request.params.rapperName.toLowerCase()
-	if(rappers[rappersName]){
-		response.json(rappers[rappersName])
+app.get('/api/:name', (request, response) => {
+	const rapperName = request.params.name.toLowerCase()
+	if(rappers[rapperName]){
+		response.json(rappers[rapperName])
 	}else{
 		response.json(rappers['dylan'])
 	}
